b1fa836213ef51131ee50c37cae5158eb4761438
AkyunaAkish <akyunaakish@gmail.com>
2015-08-23 15:07:13 -0600

    added remote

---
+++ public/js/routes.js
@@ -0,0 +1,10 @@
+app.config(function($routeProvider, $locationProvider){
+
+  $routeProvider
+  .when('/', {
+    templateUrl: 'partials/home.html',
+    controller: 'HomeController'
+  }).otherwise({redirectTo:"/"});
+
+  $locationProvider.html5Mode(true);
+});
